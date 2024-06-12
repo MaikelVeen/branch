352ebd9e107385f52b48e5ab621f04001e0528d0
Maikel Veen <veenmaikel@gmail.com>
2024-06-12 21:00:47 +0200

    feat: Add configuration

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -3,14 +3,23 @@ package cmd
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/MaikelVeen/branch/pkg/cmd/jira"
 	"github.com/MaikelVeen/branch/pkg/cmd/jira/auth"
 	"github.com/lmittmann/tint"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultConfigFilename = ".branchconfig"
+	envPrefix             = "BRANCH"
 )
 
 var rootCmd = &cobra.Command{
@@ -18,6 +27,10 @@ var rootCmd = &cobra.Command{
 	Short: "branch is a VSC and Jira swiss army knife",
 	Long:  "branch offers multiple commands to make your life easier when working with version control systems and Jira.",
 	PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+		if err := initializeConfig(cmd); err != nil {
+			return err
+		}
+
 		authCtx, err := auth.LoadUserContext()
 		if err != nil {
 			if errors.Is(err, auth.ErrAuthContextMissing) {
@@ -61,4 +74,39 @@ func runParentPersistentPreRun(cmd *cobra.Command, args []string) {
 			parent.PersistentPreRun(parent, args)
 		}
 	}
+}
+
+func initializeConfig(cmd *cobra.Command) error {
+	v := viper.New()
+
+	v.SetConfigName(defaultConfigFilename)
+	v.AddConfigPath("$HOME")
+	v.AddConfigPath(".")
+
+	var cfgNotFoundError viper.ConfigFileNotFoundError
+	if err := v.ReadInConfig(); err != nil {
+		if !errors.As(err, &cfgNotFoundError) {
+			return err
+		}
+	}
+
+	v.SetEnvPrefix(envPrefix)
+	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	v.AutomaticEnv()
+
+	bindFlags(cmd, v)
+
+	return nil
+}
+
+// Bind each cobra flag to its associated viper configuration (config file and environment variable).
+func bindFlags(cmd *cobra.Command, v *viper.Viper) {
+	cmd.Flags().VisitAll(func(f *pflag.Flag) {
+		configName := f.Name
+
+		if !f.Changed && v.IsSet(configName) {
+			val := v.Get(configName)
+			_ = cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
 }
